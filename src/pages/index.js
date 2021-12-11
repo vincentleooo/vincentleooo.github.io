39f93ecfc79ceeb8453e57d83d7d33565987949c
Vincent Leonardo <vincentleoooo@icloud.com>
2021-12-11 20:11:33 +0800

    Added SMA model content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,10 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-	  <StaticImage alt="Leong Wing Sie" src="../images/2021-10-25-26-Wing-Sie-Xiu-Hui-1.jpg"/>
+      <p>Highlighted projects:</p>
+      <ul>
+        <li><a href="https://github.vinleonardo.com/sma-airport-model/">Airport arrivals agent-based simulation in a post-pandemic world</a></li>
+      </ul>
     </Layout>
   )
 }
